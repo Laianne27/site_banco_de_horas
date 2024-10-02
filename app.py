fe907ecce49b2c106c5313fb45b1773d3c8f8b79
Laianne27 <laianne.cst27@gmail.com>
2024-10-01 21:07:42 -0300

    Atualização no app

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import datetime
 
 # Configurando o layout da página como "wide" para usar toda a largura da tela
 st.set_page_config(layout="wide")
@@ -14,10 +11,6 @@ hora_entrada = st.time_input("Horário de Entrada")
 hora_saida = st.time_input("Horário de Saída")
 pausa = st.time_input("Pausa")
 
-if st.button("Registrar"):
-    # Adiciona os dados na planilha
-    sheet.append_row([colaborador, str(data), str(hora_entrada), str(hora_saida), str(pausa)])
-    st.success("Batida registrada com sucesso!")
 
 
 
