698276248df167d5c97f19184bc64dcf98d803db
Laianne27 <laianne.cst27@gmail.com>
2024-10-27 15:04:44 -0300

    commit changes

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import pandas as pd
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 # Carregue suas credenciais
-creds = ServiceAccountCredentials.from_json_keyfile_name("C:\Users\Cliente\Downloads\site-cq-moinho-727d76d37cc2.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(r"C:\Users\Cliente\Documents\Tecnologia\Python\site-cq-moinho-727d76d37cc2.json", scope)
+
 
 # Autorize o cliente gspread
 gc = gspread.authorize(creds)
